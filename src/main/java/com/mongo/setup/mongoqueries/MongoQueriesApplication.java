44da535e4101cc13393f1ec2d206017382eaab5f
Bishal Jaiswal <jaiswalbishal3@gmail.com>
2018-12-26 22:52:11 +0530

    Setting up new project

---
+++ src/main/java/com/mongo/setup/mongoqueries/MongoQueriesApplication.java
@@ -0,0 +1,14 @@
+package com.mongo.setup.mongoqueries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoQueriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoQueriesApplication.class, args);
+	}
+
+}
+
